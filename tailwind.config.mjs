7231adf76876a614d747cfb8cca59599a32ed111
lucas id betan <lucas.idbetan@gmail.com>
2024-05-08 20:28:11 -0300

    Initialized project, installed Tailwind & Flowbite, added Navbar, Footer.

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+		"./node_modules/flowbite/**/*.js",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				mountains: "url(/assets/tianzi-1.jpg)",
+			},
+			colors: {
+				background: "#FAF8F0",
+				primary: "#E2D8B1",
+				secondary: "#52AFA5",
+				accent: "#EF9932",
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
