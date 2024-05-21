2f741eccd138c972b7f842de5adb6e69118347a9
Lucas Id Betan <lucas.idbetan@gmail.com>
2024-05-21 18:25:39 -0300

    mountains bg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,24 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				mountains: "url(/assets/tianzi-1.jpg)",
+				mountains1: "url(/assets/mountain_1.png)",
+				mountains2: "url(/assets/mountain_2.png)",
+				mountains3: "url(/assets/mountain_3.png)",
+				mountains4: "url(/assets/mountain_4.png)",
+				mountains5: "url(/assets/mountain_5.png)",
+				mountains6: "url(/assets/mountain_6.png)",
+				mountains7: "url(/assets/mountain_7.png)",
+				mountains8: "url(/assets/mountain_8.png)",
+				mountains9: "url(/assets/mountain_9.png)",
+				mountains10: "url(/assets/mountain_10.png)",
+				fog1: "url(/assets/fog_1.png)",
+				fog2: "url(/assets/fog_2.png)",
+				fog3: "url(/assets/fog_3.png)",
+				fog4: "url(/assets/fog_4.png)",
+				fog5: "url(/assets/fog_5.png)",
+				fog6: "url(/assets/fog_6.png)",
+				fog7: "url(/assets/fog_7.png)",
+				sun: "url(/assets/sun_rays.png)",
 			},
 			colors: {
 				background: "#FAF8F0",
